0df879dd559abf50ef2d4ff7870fcc74ba888573
Harshvardhan Singh <84776690+cyberskull99297@users.noreply.github.com>
2025-10-11 23:39:09 +0530

    Utils file addition

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ dotenv.config({
 })
 
 connectDb()
+.then(() => {
+    app.on("error", (error) => {
+            console.log("Error:", error);
+            throw error
+        })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed !!!!". error)
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ********* iffy use to connect MONGODB******************
 // import express from "express";
 // const app = express()
 
@@ -46,3 +46,4 @@ connectDb()
 //         throw error
 //     }
 // })()
+// **************************************************
